252b1d3753b25179a7ffc60a095d71bfabfcf9ce
Myroslav Rudnytskyi <myroslav.rudnytskyi@gmail.com>
2016-06-18 13:11:33 +0300

    Update database dependencies (add Mongo database and Gson - library for Java objects/JSON documents mapping)

---
+++ src/main/java/org/test/storage/MongoRepository.java
@@ -0,0 +1,11 @@
+package org.test.storage;
+
+/**
+ * Class provides access to the <a href="https://www.mongodb.com">MongoDB</a> storage using
+ * <a href="https://github.com/google/gson">Gson</a> library.
+ *
+ * @author Myroslav Rudnytskyi
+ * @version 18.06.2016
+ */
+public class MongoRepository {
+}
